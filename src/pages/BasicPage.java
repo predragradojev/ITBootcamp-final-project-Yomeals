19e381d9b263791bf2df79fe31cb071dca5b6849
Predrag Radojev <pedjaradojev@gmail.com>
2021-10-18 09:42:17 +0200

    Change BasicPage

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -2,14 +2,15 @@ package pages;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasicPage {
-	
+
 	protected WebDriver driver;
-	protected WebDriver wait;
+	protected WebDriverWait wait;
 	protected JavascriptExecutor js;
 
-	public BasicPage(WebDriver driver, WebDriver wait, JavascriptExecutor js) {
+	public BasicPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
 		super();
 		this.driver = driver;
 		this.wait = wait;
